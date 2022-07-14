75c4f14de502d3d830284f7b2695c12dc6595f63
johol355 <johol355@gmail.com>
2022-07-14 16:27:38 +0200

    first commit subproject 2

--- helloworld.R
+++ helloworld.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 print('helo wordl!')
 print('THE MERGING OF WORLDS (OR BRANCHES) IS NEIGH')
 
@@ -6,3 +7,6 @@ print('this is a piece of code im certain will work')
 print('this is another pience of code that WILL work')
 
 print('this as well, commited without usin commit amend...')
+=======
+print('helo wordl!')
+>>>>>>> parent of ce0bd45 (addition)
